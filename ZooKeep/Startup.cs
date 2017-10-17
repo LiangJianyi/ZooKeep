91e8f97793e0c08a9b4dd9793d0e953a0c3d99e4
建溢 梁 <a.124p@live.com>
2017-10-18 02:08:17 +0800

    添加项目文件。

---
+++ ZooKeep/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZooKeep.Startup))]
+namespace ZooKeep
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
