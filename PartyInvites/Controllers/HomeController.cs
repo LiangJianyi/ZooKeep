382d2d10c3e34333def8c312dd1096ae7795e802
建溢 梁 <a.124p@live.com>
2017-10-18 23:50:05 +0800

    添加“测试环境”分支
    在该分支下创建了 PartyInvites 项目

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers {
+	public class HomeController : Controller {
+		// GET: Home
+		public ActionResult Index( ) {
+			ViewBag.Fuck = "Fuck";
+			ViewBag.Bitch = "Bitch";
+			return View( );
+		}
+	}
+}
